6ca0e47dc45c415e72b218c90214a3a8056f6dd2
Nirma Nethmini <nirmanethmini@gmail.com>
2025-07-19 20:40:55 +0530

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,3 +1,7 @@
 def encrypt(text, shift):
     result = ""
     for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            shifted = (ord(char) - base + shift) % 26 + base
+            result += chr(shifted)
